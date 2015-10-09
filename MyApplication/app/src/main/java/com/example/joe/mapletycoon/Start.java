af615851a9a48d55a79754d09e57514a243a8cea
Joe Caruso <joseph.caruso@mymail.champlain.edu>
2015-10-09 17:11:20 -0400

    xml setup

---
+++ MyApplication/app/src/main/java/com/example/joe/mapletycoon/Start.java
@@ -0,0 +1,21 @@
+package com.example.joe.mapletycoon;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Joe on 10/9/2015.
+ */
+public class Start extends MainActivity {
+
+
+    public void setName
+    {
+        
+
+
+
+    }
+
+
+
+}
